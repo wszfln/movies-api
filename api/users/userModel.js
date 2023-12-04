6216cc148fa453c39076b717a317ddb6ac72aa64
Yingying Lu <20095278@mail.wit.ie>
2023-12-04 15:43:17 +0000

    Initialise Dev Env - Data

---
+++ api/users/userModel.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: { type: String, unique: true, required: true},
+  password: {type: String, required: true }
+});
+
+const passwordValidator = (password) => {
+    let pattern = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+    password.match(pattern);;
+}
+UserSchema.path("password").validate(passwordValidator);
+
+export default mongoose.model('User', UserSchema);
