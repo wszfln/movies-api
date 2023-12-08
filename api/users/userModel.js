f453e2f4b58d16afb4fa33c568a5d335bb44c7c9
Yingying Lu <20095278@mail.wit.ie>
2023-12-08 00:50:07 +0000

    Salt and Hash

--- api/users/userModel.js
+++ api/users/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const Schema = mongoose.Schema;
 
@@ -7,9 +8,34 @@ const UserSchema = new Schema({
   password: {type: String, required: true }
 });
 
+UserSchema.methods.comparePassword = async function (passw) { 
+  return await bcrypt.compare(passw, this.password); 
+}
+
+UserSchema.statics.findByUserName = function (username) {
+  return this.findOne({ username: username });
+};
+
+UserSchema.pre('save', async function(next) {
+  const saltRounds = 10; // You can adjust the number of salt rounds
+  //const user = this;
+  if (this.isModified('password') || this.isNew) {
+    try {
+      const hash = await bcrypt.hash(this.password, saltRounds);
+      this.password = hash;
+      next();
+  } catch (error) {
+     next(error);
+  }
+
+  } else {
+      next();
+  }
+});
+
 const passwordValidator = (password) => {
     let pattern = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
-    password.match(pattern);;
+    password.match(pattern);
 }
 UserSchema.path("password").validate(passwordValidator);
 
